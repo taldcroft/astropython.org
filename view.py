7ff0007c0e71ffc6d814b970fcd3718318332203
Tom Aldcroft <taldcroft@gmail.com>
2009-12-07 14:27:37 -0500

    - Temporarily disable listing of all tags due to incompatibility with AE 1.2.8. - Allow remote API access via app.yaml

--- view.py
+++ view.py
@@ -169,7 +169,8 @@ class ViewPage(object):
             NUM_FULL_RENDERS[path] = 0
         NUM_FULL_RENDERS[path] += 1     # This lets us see % of cached views
                                         # in /admin/timings (see timings.py)
-        tags = Tag.list()
+        # tags = sorted(Tag.list(), key=lambda x: x['name'].lower())
+        tags = []  # Tag.list()
 
         # Define some parameters it'd be nice to have in views by default.
         template_params = {
